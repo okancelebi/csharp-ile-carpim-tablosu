bab07da840710ea44c9023bc2ac1de4fce1367af
Okan Çelebi <okancelebi06@gmail.com>
2022-12-29 15:21:09 +0300

    Add files via upload

---
+++ csharp ile çarpım tablosu/csharp ile çarpım tablosu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ile_çarpım_tablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Bu projemizde for döngüsü ile çarpım tablosu.
+                for (int i = 1; i <= 10; i++)
+                {
+                    for (int j = 1; j <= 10; j++)
+                    {
+                        Console.WriteLine(i + "x" + j + "=" + (i * j));
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
